a12b81081bcd169d7b6b8f6913097b7680a586c6
Leonhardt Koepsell <leonhardt@koepsell.io>
2017-06-14 08:53:19 -0500

    Exit script if any contained command fails

--- release/download_blobs.sh
+++ release/download_blobs.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 mkdir -p blobs/oraclejdk8 blobs/hbase blobs/kafka
 
 (cd blobs/oraclejdk8; curl -b "oraclelicense=accept-securebackup-cookie" -L -J -C - -O http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.tar.gz)
